35f2a949e41b569d387561b9220aacc603f935e7
phanchicuong <142389353+phanchicuong@users.noreply.github.com>
2023-09-06 16:14:13 +0700

    Update Program

--- bai7/Program.cs
+++ bai7/Program.cs
@@ -15,7 +15,7 @@ namespace Bai07
             sc.X = 5;
             sc.Y = 5;
             HinhTron ht = new HinhTron(sc, 10.5);
-            Console.WriteLine("Hinh tron co tam la{0}({1},{2}) voi ban kinh la {3} co dien tich va chu vi la {4} và {5}", td.Ten, td.X, td.Y, ht.BanKinh, Math.Round(ht.tinhDienTich(), 3), Math.Round(ht.tinhChuVi(), 3));
+            Console.WriteLine("Hinh tron co tam la{0}({1},{2}) voi ban kinh la {3} co dien tich va chu vi la {4} và {5}", sc.Ten, sc.X, sc.Y, ht.BanKinh, Math.Round(ht.tinhDienTich(), 3), Math.Round(ht.tinhChuVi(), 3));
             Console.ReadLine();
         }
     }
